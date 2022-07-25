0395e906253764b084f5a89525368b2fe1190058
nilesh.devdas <nilesh.devdas@vinsys.com>
2022-07-25 16:54:36 +0530

    Sample Debug code

--- src/main/java/com/vinsys/airports/SampleIOExample.java
+++ src/main/java/com/vinsys/airports/SampleIOExample.java
@@ -1,4 +1,33 @@
 package com.vinsys.airports;
 
+
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
 public class SampleIOExample {
+
+    public void addNextPaymentCall(int a , int b){
+        computeInterest(a, b, 15);
+    }
+
+    private double computeInterest(int a, int b, int i) {
+        int defaulterOccurences = fetchDefaultPayments();
+        if(defaulterOccurences > 0){
+            return defaulterOccurences * 500;
+        }else{
+            return 0;
+        }
+    }
+
+    private int fetchDefaultPayments() {
+        System.out.println("new customer no occurences...");
+        return 0;
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        SampleIOExample example = new SampleIOExample();
+        example.addNextPaymentCall(100,100);
+    }
 }
